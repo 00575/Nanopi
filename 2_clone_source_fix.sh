0112bb3396c17134e8c550946a25b55f64effdd2
My-Compile <75769819+My-Compile@users.noreply.github.com>
2021-02-22 11:12:37 +0800

    Add files via upload

---
+++ 2_clone_source_fix.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/friendlyarm/repo
+sudo cp repo/repo /usr/bin/
+mkdir friendlywrt-$2
+cd friendlywrt-$2
+repo init -u https://github.com/biliwala/friendlywrt_manifests -b $1 -m $2.xml --repo-url=https://github.com/friendlyarm/repo --no-clone-bundle --depth=1
+repo sync -c --no-tags --no-clone-bundle -j8
+cd friendlywrt/ && git fetch --unshallow
+#cd friendlywrt/ && git remote rename self origin && git fetch --unshallow
+git checkout `git branch -va | grep remotes/m | awk '{print $3}' | awk -F\/ '{print $2}'`
+#git checkout -b master-v19.07.1 origin/master-v19.07.1
+
+[ -f target/linux/rockchip-rk3328/config-4.14 ] && sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/rockchip-rk3328/config-4.14
+[ -f target/linux/allwinner-h5/config-4.14 ] &&    sed -i '/CONFIG_CGROUPS/a\CONFIG_CGROUP_PERF=y' target/linux/allwinner-h5/config-4.14
+git add . && git commit -m 'reset'
+
+cd ../ && find device/ -name distfeeds.conf -delete
