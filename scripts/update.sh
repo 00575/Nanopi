31e2cb86a50263756e07ed949c08b735d30cd336
My-Compile <75769819+My-Compile@users.noreply.github.com>
2021-03-08 15:28:30 +0800

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 set -e $1
 
+opkg update || true
 if ! command -v pv &> /dev/null
 then
-	opkg update || true
 	opkg install pv
-	if ! command -v pv &> /dev/null; then echo -e '\e[91mpv命令无效，升级中止！\e[0m' && exit 1; fi
+	if ! command -v pv &> /dev/null; then echo -e '\e[91mpv命令不可用，升级中止！\e[0m' && exit 1; fi
 fi
 if ! command -v fdisk &> /dev/null
 then
-	opkg update || true
 	opkg install --force-overwrite fdisk
-	if ! command -v fdisk &> /dev/null; then echo -e '\e[91mfdisk命令无效，升级中止！\e[0m' && exit 1; fi
+	if ! command -v fdisk &> /dev/null; then echo -e '\e[91mfdisk命令不可用，升级中止！\e[0m' && exit 1; fi
+fi
+if ! command -v losetup &> /dev/null
+then
+	opkg install --force-overwrite losetup
+	if ! command -v losetup &> /dev/null; then echo -e '\e[91mlosetup命令不可用，升级中止！\e[0m' && exit 1; fi
 fi
 
 board_id=$(cat /etc/board.json | jsonfilter -e '@["model"].name' | tail -c 4 | tr -d "\n" | awk '{print tolower($0)}')
 mount -t tmpfs -o remount,size=650m tmpfs /tmp
 rm -rf /tmp/upg && mkdir /tmp/upg && cd /tmp/upg
-wget https://ghproxy.com/https://github.com/My-Compile/nanopi-R2S/download/$board_id-$(date +%Y-%m-%d)/$board_id.img.gz || true
+wget https://ghproxy.com/https://github.com/My-Compile/nanopi-R2S/releases/download/$board_id-$(date +%Y-%m-%d)/$board_id.img.gz || true
 if [ -f $board_id.img.gz ]; then
 	wget https://ghproxy.com/https://github.com/My-Compile/nanopi-R2S/releases/download/$board_id-$(date +%Y-%m-%d)/md5sum.txt
 	echo -e '\e[92m今天固件已下载，准备解压\e[0m'
@@ -44,9 +48,11 @@ fi
 	pv $board_id.img.gz | gunzip -dc > FriendlyWrt.img && rm $board_id.img.gz
 
 #fi
+lodev=$(losetup -f)
 offset=`expr $(fdisk -l -u FriendlyWrt.img | tail -n1 | awk '{print $2}') \* 512`
+losetup -o $offset $lodev FriendlyWrt.img
 mkdir -p /mnt/img
-mount -o loop,offset=$offset FriendlyWrt.img /mnt/img
+mount $lodev /mnt/img
 echo -e '\e[92m解压已完成，准备编辑镜像文件，写入备份信息\e[0m'
 cd /mnt/img
 sysupgrade -b back.tar.gz
@@ -57,6 +63,7 @@ echo -e '\e[92m备份文件已经写入，移除挂载\e[0m'
 #rm back.tar.gz
 cd /tmp/upg
 umount /mnt/img
+losetup -d $lodev
 echo -e '\e[92m正在打包...\e[0m'
 #zstdmt /mnt/mmcblk0p2/FriendlyWrt.img -o /tmp/FriendlyWrtupdate.img.zst
 echo -e '\e[92m开始写入，请勿中断...\e[0m'
