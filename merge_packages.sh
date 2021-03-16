f141c2f957815ef6d755b0dc6a5b473d381cbaff
My-Compile <75769819+My-Compile@users.noreply.github.com>
2021-03-16 20:30:21 +0800

    Update merge_packages.sh

--- merge_packages.sh
+++ merge_packages.sh
@@ -15,9 +15,12 @@ function merge_package(){
     mv $pn package/
 }
 
+mkdir -p package/base-files/files/etc/dropbear
+mv $GITHUB_WORKSPACE/host_keys/* package/base-files/files/etc/dropbear/
+chmod 600 package/base-files/files/etc/dropbear/*
+
 merge_package https://github.com/linkease/ddnsto-openwrt
 merge_package https://github.com/project-lede/luci-app-godproxy
-merge_package https://github.com/jerrykuku/luci-theme-argon luci-theme-argon "-b 18.06"
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifsd luci-app-ksmbd
 merge_package https://github.com/coolsnowwolf/packages/trunk/kernel/ksmbd
 merge_package https://github.com/coolsnowwolf/packages/trunk/net/ksmbd-tools
@@ -31,11 +34,9 @@ wget https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux
 chmod +x target/linux/rockchip/armv8/base-files/etc/init.d/fa-rk3328-pwmfan
 mkdir -p target/linux/rockchip/armv8/base-files/etc/rc.d &&\
 ln -sf ../init.d/fa-rk3328-pwmfan target/linux/rockchip/armv8/base-files/etc/rc.d/S96fa-rk3328-pwmfan
-sed -i "s/enable\ \'0\'/enable \'1\'/" package/ctcgfw/luci-app-oled/root/etc/config/oled
+sed -i "s/enable '0'/enable '1'/" package/ctcgfw/luci-app-oled/root/etc/config/oled
 fi
 
+sed -i "/\/etc\/coremark\.sh/d" package/feeds/packages/coremark/Makefile
+sed -i "s/option limit_enable '1'/option limit_enable '0'/" package/lean/nft-qos/files/nft-qos.config
 sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
-
-mkdir -p package/base-files/files/etc/dropbear
-mv $GITHUB_WORKSPACE/host_keys/* package/base-files/files/etc/dropbear/
-chmod 600 package/base-files/files/etc/dropbear/*
