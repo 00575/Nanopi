6d9f428710fcb93e18f2e540eedd924d5b0b1620
a0575 <68583179+a0575@users.noreply.github.com>
2021-05-06 08:46:35 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -20,21 +20,19 @@ if [ $DEVICE = 'r2s' ]; then
     echo -e "\toption maxfreq0 '1512000'\n" >> package/lean/luci-app-cpufreq/root/etc/config/cpufreq
 fi
 
-#if [ $DEVICE = 'r4s' ]; then
-    #wget https://github.com/immortalwrt/immortalwrt/commit/6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #sed -i 's/ctcgfw/kernel/g' 6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #git apply 6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #rm 6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
-#fi
+if [ $DEVICE = 'r4s' ]; then
+    wget https://github.com/immortalwrt/immortalwrt/commit/6c3f6d2686679173b95495c47d861db1f41729dd.patch
+    sed -i 's/package\/ctcgfw\/r8168\///g' 6c3f6d2686679173b95495c47d861db1f41729dd.patch
+    mv 6c3f6d2686679173b95495c47d861db1f41729dd.patch package/kernel/r8168/patches/099-6c3f6d2686679173b95495c47d861db1f41729dd.patch
+    sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
+fi
 
-#if [[ $DEVICE =~ ('r2s'|'r4s'|'r1p') ]]; then
-    #wget https://github.com/coolsnowwolf/lede/raw/757e42d70727fe6b937bb31794a9ad4f5ce98081/target/linux/rockchip/config-default -NP target/linux/rockchip/
-    #wget https://github.com/coolsnowwolf/lede/blob/910af7e482376ee32006a71168961236cbdf8c29/target/linux/rockchip/config-default -NP target/linux/rockchip/
-    #wget https://github.com/coolsnowwolf/lede/commit/f341ef96fe4b509a728ba1281281da96bac23673.patch
-    #git apply f341ef96fe4b509a728ba1281281da96bac23673.patch
-    #rm f341ef96fe4b509a728ba1281281da96bac23673.patch
-#fi
+if [[ $DEVICE =~ ('r2s'|'r4s'|'r1p') ]]; then
+    wget https://github.com/coolsnowwolf/lede/raw/757e42d70727fe6b937bb31794a9ad4f5ce98081/target/linux/rockchip/config-default -NP target/linux/rockchip/
+    wget https://github.com/coolsnowwolf/lede/commit/f341ef96fe4b509a728ba1281281da96bac23673.patch
+    git apply f341ef96fe4b509a728ba1281281da96bac23673.patch
+    rm f341ef96fe4b509a728ba1281281da96bac23673.patch
+fi
 
 sed -i '/182.140.223.146/d' scripts/download.pl
 sed -i '/\.cn\//d' scripts/download.pl
