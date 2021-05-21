69b104b7d471eb8ed0f1b3eb7f8cbff8a08002cd
a0575 <68583179+a0575@users.noreply.github.com>
2021-05-21 12:25:11 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -20,12 +20,12 @@ if [ $DEVICE = 'r2s' ]; then
     echo -e "\toption maxfreq0 '1512000'\n" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
 fi
 
-#if [ $DEVICE = 'r4s' ]; then
-    #wget https://github.com/immortalwrt/immortalwrt/commit/6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #sed -i 's/package\/ctcgfw\/r8168\///g' 6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #mv 6c3f6d2686679173b95495c47d861db1f41729dd.patch package/kernel/r8168/patches/099-6c3f6d2686679173b95495c47d861db1f41729dd.patch
-    #sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
-#fi
+if [ $DEVICE = 'r4s' ]; then
+    wget https://github.com/immortalwrt/immortalwrt/commit/6c3f6d2686679173b95495c47d861db1f41729dd.patch
+    sed -i 's/package\/ctcgfw\/r8168\///g' 6c3f6d2686679173b95495c47d861db1f41729dd.patch
+    mv 6c3f6d2686679173b95495c47d861db1f41729dd.patch package/kernel/r8168/patches/099-6c3f6d2686679173b95495c47d861db1f41729dd.patch
+    sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
+fi
 
 #if [[ $DEVICE =~ ('r2s'|'r4s'|'r1p') ]]; then
     #wget https://github.com/coolsnowwolf/lede/raw/757e42d70727fe6b937bb31794a9ad4f5ce98081/target/linux/rockchip/config-default -NP target/linux/rockchip/
