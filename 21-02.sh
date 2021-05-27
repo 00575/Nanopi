8c63689d3ed090c4448229cbbadca80b6d27c9fd
a0575 <68583179+a0575@users.noreply.github.com>
2021-05-27 19:33:12 +0800

    Update 21-02.sh

--- 21-02.sh
+++ 21-02.sh
@@ -39,7 +39,7 @@ sed -i '/\.cn\//d' scripts/download.pl
 sed -i '/tencent/d' scripts/download.pl
 
 sed -i 's/ImmortalWrt/#/' package/base-files/files/bin/config_generate
-sed -i '/distname/d' package/emortal/autocore/files/*/index.htm
+#sed -i '/distname/d' package/emortal/autocore/files/*/index.htm
 sed -i '/distname/d' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i '/Source Code/,+2d' package/emortal/autocore/files/*/index.htm
 sed -i '/Source Code/,+2d' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
