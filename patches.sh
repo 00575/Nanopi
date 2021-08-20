6a4fbba3f6a2f45e545ae0b12bb65eafd36a1007
a0575 <68583179+a0575@users.noreply.github.com>
2021-08-20 17:43:05 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -15,13 +15,14 @@ sed -i '/DEPENDS+/ s/$/ +wsdd2/' `find package/ -follow -type f -path '*/ksmbd-t
 sed -i 's/ +ntfs-3g/ +ntfs3-mount/' `find package/ -follow -type f -path '*/automount/Makefile'`
 sed -i '/skip\=/ a skip=`mount | grep -q /dev/$device; echo $?`' `find package/ -follow -type f -path */automount/files/15-automount`
 
-if [ $DEVICE = 'r2s' ]; then
-    sed -i "s/enable '0'/enable '1'/" `find feeds/ -type f -name oled | grep config`
+if [ $DEVICE = 'r2s' -o $DEVICE = 'r2c' ]; then
     sed -i 's/1400000/1450000/' target/linux/rockchip/patches-5.4/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
-    truncate -s-1 package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
-    echo -e "\toption governor0 'schedutil'" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
-    echo -e "\toption minfreq0 '816000'" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
-    echo -e "\toption maxfreq0 '1512000'\n" >> package/feeds/luci/luci-app-cpufreq/root/etc/config/cpufreq
+    sed -i "s/enable '0'/enable '1'/" `find package/ -follow -type f -path '*/luci-app-oled/root/etc/config/oled'`
+    config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
+    truncate -s-1 $config_file_cpufreq
+    echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
+    echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
+    echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
 fi
 
 if [ $DEVICE = 'r4s' ]; then
@@ -31,7 +32,7 @@ if [ $DEVICE = 'r4s' ]; then
     sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
 fi
 
-if [[ $DEVICE =~ ('r2s'|'r4s'|'r1p') ]]; then
+if [[ $DEVICE =~ ('r2s'|'r2c'|'r4s'|'r1p') ]]; then
     wget https://github.com/coolsnowwolf/lede/raw/757e42d70727fe6b937bb31794a9ad4f5ce98081/target/linux/rockchip/config-default -NP target/linux/rockchip/
     wget https://github.com/coolsnowwolf/lede/commit/f341ef96fe4b509a728ba1281281da96bac23673.patch
     git apply f341ef96fe4b509a728ba1281281da96bac23673.patch
