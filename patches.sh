7ad36220ee4c5aa544d1fddbe1d57ef8c619babc
a0575 <68583179+a0575@users.noreply.github.com>
2021-08-30 11:12:54 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -75,9 +75,9 @@ sed -i '/182.140.223.146/d;/\.cn\//d;/tencent/d' scripts/download.pl
 
 sed -i 's/ImmortalWrt/Nanopi/' package/base-files/files/bin/config_generate
 #sed -i '/distname/d' package/emortal/autocore/files/*/index.htm
-sed -i '/distname/d' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i '/Source Code/,+2d' package/emortal/autocore/files/*/index.htm
-sed -i '/Source Code/,+2d' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#sed -i '/distname/d' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#sed -i '/Source Code/,+2d' package/emortal/autocore/files/*/index.htm
+#sed -i '/Source Code/,+2d' ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i '/Powered by/i\<strong>Nanopi Build For github </strong><a href="https://github.com/00575/Nanopi" target="_blank"><strong>00575</strong></a>' ./feeds/luci/themes/*/luasrc/view/themes/*/footer.htm
 sed -i '/Powered by/,+12d' ./feeds/luci/themes/*/luasrc/view/themes/*/footer.htm
 
