4b577d059c278d4af84855b1a1579336cd8923fd
a0575 <68583179+a0575@users.noreply.github.com>
2021-10-21 15:28:08 +0800

    Update autoupdate-bash.sh

--- scripts/autoupdate-bash.sh
+++ scripts/autoupdate-bash.sh
@@ -12,9 +12,6 @@ proceed_command sfdisk
 proceed_command losetup
 proceed_command resize2fs
 opkg install coreutils-truncate || true
-wget -NP /tmp https://ghproxy.com/https://raw.githubusercontent.com/00575/Nanopi/zstd-bin/truncate
-wget -NP /tmp https://ghproxy.com/https://raw.githubusercontent.com/00575/Nanopi/zstd-bin/ddnz
-chmod +x /tmp/truncate /tmp/ddnz
 
 board_id=$(cat /etc/board.json | jsonfilter -e '@["model"].id' | sed 's/friendly.*,nanopi-//;s/xunlong,orangepi-//;s/^r1s-h5$/r1s/;s/^r1$/r1s-h3/;s/^r1-plus$/r1p/;s/default-string-default-string/x86/')
 mount -t tmpfs -o remount,size=850m tmpfs /tmp
@@ -43,7 +40,7 @@ if [ $md5r != $md5sum ]; then
 fi
 
 mv $board_id.img FriendlyWrt.img
-[ $board_id = 'x86' ] && drive='sda' || device='mmcblk0'
+[ $board_id = 'x86' ] && drive='sda' || drive='mmcblk0'
 bs=`expr $(cat /sys/block/$drive/size) \* 512`
 truncate -s $bs FriendlyWrt.img || ../truncate -s $bs FriendlyWrt.img
 echo ", +" | sfdisk -N 2 FriendlyWrt.img
@@ -57,6 +54,8 @@ sleep 10
 cd /mnt/img
 sysupgrade -b back.tar.gz
 tar zxf back.tar.gz
+opkg list-installed | grep "luci-i18n\|luci-app" | cut -d\  -f1 | sort -r | xargs -n1 echo opkg install > packages_needed
+sed -i '/exit/i\[ -e /packages_needed ] && (mv /packages_needed /packages_needed.installed && sh /packages_needed.installed)\' /etc/rc.local
 if ! grep -q macaddr /etc/config/network; then
 	echo -e '\e[91m注意：由于已知的问题，“网络接口”配置无法继承，重启后需要重新设置WAN拨号和LAN网段信息\e[0m'
 	rm etc/config/network;
@@ -65,21 +64,3 @@ echo -e '\e[92m备份文件已经写入，移除挂载\e[0m'
 #rm back.tar.gz
 cd /tmp/upg
 umount /mnt/img
-
-sleep 5
-if cat /proc/mounts | grep -q ${lodev}p1; then umount ${lodev}p1; fi
-if cat /proc/mounts | grep -q ${lodev}p2; then umount ${lodev}p2; fi
-e2fsck -yf ${lodev}p2 || true
-resize2fs ${lodev}p2
-
-losetup -d $lodev
-echo -e '\e[92m正在打包...\e[0m'
-echo -e '\e[92m开始写入，请勿中断...\e[0m'
-if [ -f FriendlyWrt.img ]; then
-	echo 1 > /proc/sys/kernel/sysrq
-	echo u > /proc/sysrq-trigger && umount / || true
-	#pv FriendlyWrt.img | dd of=/dev/mmcblk0 conv=fsync
-	dd if=FriendlyWrt.img of=/dev/$drive conv=sparse status=progress
-	echo -e '\e[92m刷机完毕，正在重启...\e[0m'
-	echo b > /proc/sysrq-trigger
-fi
