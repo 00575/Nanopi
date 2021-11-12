f41df3071dfca7ae6728792264ae6d12c05db0c0
00575 <xixiaaijia@gmail.com>
2021-11-12 20:08:28 +0000

    update-2021.11.12

--- scripts/patches_5.10.x.sh
+++ scripts/patches_5.10.x.sh
@@ -11,6 +11,7 @@ sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
 cp -f $GITHUB_WORKSPACE/scripts/banner package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/scripts/banner package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/scripts/banner package/base-files/files/etc/banner
+cp -f $GITHUB_WORKSPACE/scripts/banner package/base-files/files/etc/banner
 sed -i 's/=1/=0/g' package/kernel/linux/files/sysctl-br-netfilter.conf
 
 sed -i '/DEPENDS/ s/$/ +libcap-bin/' `find package/ -follow -type f -path '*/luci-app-openclash/Makefile'`
